d99c9b7f39de9e0dc54e97d7d123a03ceef21c0f
cristianat98 <cristianarmestotejedo@gmail.com>
2020-05-06 13:45:39 +0200

    Acera finalizada

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class BoardManager : MonoBehaviour
 {
     public GameObject acera;
-    private Transform boardHolder;	
+    private Transform boardHolder;
+    public List<GameObject> carretera;
 
     public void SetupScene()
     {
         boardHolder = new GameObject("Board").transform;
-        for (float x = -4.5f; x < 5.5; x++)
+        for (float x = -49.5f; x < 10.5; x++)
         {
-            for (float y = -4.5f; y < 5.5; y++)
-            {
-                GameObject instance = Instantiate(acera, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+            for (float y = -9.5f; y < 10.5; y++)
+            {   
+                GameObject instance = Instantiate(acera, new Vector3(x, y, 0f), Quaternion.identity);
                 instance.transform.SetParent(boardHolder);
             }
         }
