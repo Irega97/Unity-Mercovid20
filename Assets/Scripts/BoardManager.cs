e2d70b3a5a73f22c58598f362317af442e27ac5e
cristianat98 <cristianarmestotejedo@gmail.com>
2020-05-18 11:26:24 +0200

    Personajes Mapa 1

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -22,6 +22,7 @@ public class BoardManager : MonoBehaviour
     public GameObject[] edificio2;
     public GameObject[] edificio3;
     public GameObject[] edificio4;
+    public GameObject[] edificio5;
     public GameObject[] hospital;
     public GameObject[] tienda;
     public GameObject[] parque;
@@ -47,60 +48,72 @@ public class BoardManager : MonoBehaviour
                            "EF   Óbbbbó  Óaaaaó   EF   eee   EF   Óccccó    ddddd     EF\n" +
                            "EF                    EF         EF                       EF\n" +
                            "EF                    EF         EF                       EF\n" +
+                           "EF                    EF         EF                       EF\n" +
                            "7155555555555599555555415555G5555415555555559955555555555548\n" +
                            "7B666666666666DB666666226666H666622666666666DB666666666666D8\n" +
                            "EF            EF                            EF            EF\n" +
-                           "EF            EF                            EF            EF\n" +
-                           "EF            EF   hhhhh       ttttt        EF            EF\n" +
+                           "EF            EF   hhhhh                    EF            EF\n" +
                            "EF   Áccccá   EF   hhhhh       ttttt        EF   Áaaaaá   EF\n" +
                            "EF   Éccccé   IJ   hhhhh       ttttt        IJ   Éaaaaé   EF\n" +
                            "EF   Íccccí   EF   hhhhh       ttttt        EF   Íaaaaí   EF\n" +
-                           "EF   Óccccó   EF   hhhhh                    EF   Óaaaaó   EF\n" +
+                           "EF   Óccccó   EF   hhhhh       ttttt        EF   Óaaaaó   EF\n" +
                            "EF            EF                            EF            EF\n" +
                            "EF            EF                            EF            EF\n" +
                            "71555555555555415555G555555555555555G55555554155555555555548\n" +
-                           "7B666666666666666666H666666666666666H666666666666666666666D8\n" +
-                           "EF         ffff                         ggggg  MMMMMMMMMMMEF\n" +
-                           "EF         fffff                        ggggg  MMMMMMMMMMMEF\n" +
-                           "EFttttt    fffff                        ggggg  MMMMMMMMMMMEF\n" +
-                           "EFttttt    ffff                         ggggg  MMMMMMMMMMMEF\n" +
-                           "EFttttt                 pppppppppp      ggggg             EF\n" +
-                           "EFttttt                 pppppppppp                        EF\n" +
+                           "7B666666666666226666H666666666666666H666666622666666666666D8\n" +
+                           "EFiiiii ggggg                            ffff  MMMMMMMMMMMEF\n" +
+                           "EFiiiii ggggg                            fffff MMMMMMMMMMMEF\n" +
+                           "EFiiiii ggggg                            fffff MMMMMMMMMMMEF\n" +
+                           "EFiiiii ggggg                            ffff  MMMMMMMMMMMEF\n" +
+                           "EFiiiii ggggg           pppppppppp                        EF\n" +
                            "EF                      pppppppppp                        EF\n" +
                            "EF                      pppppppppp                        EF\n" +
                            "EF                      pppppppppp                        EF\n" +
-                           "EF        ggggg         pppppppppp                        EF\n" +
-                           "EFddddd   ggggg         pppppppppp              ffff      EF\n" +
-                           "EFddddd   ggggg         pppppppppp              fffff  eeeEF\n" +
-                           "EFddddd   ggggg                                 fffff  eeeEF\n" +
-                           "EFddddd   ggggg                                 ffff   eeeEF\n" +
+                           "EF                      pppppppppp                        EF\n" +
+                           "EF                      pppppppppp             ggggg iiiiiEF\n" +
+                           "EFddddd ffff            pppppppppp             ggggg iiiiiEF\n" +
+                           "EFddddd fffff           pppppppppp             ggggg iiiiiEF\n" +
+                           "EFddddd fffff                                  ggggg iiiiiEF\n" +
+                           "EFddddd ffff                                   ggggg iiiiiEF\n" +
                            "EF                                                        EF\n" +
                            "EF                                                        EF\n" +
                            "71555555555555995555G5555555555555555555G5559955555555555548\n" +
                            "7B666666666666DB6666H6666666666666666666H666DB666666666666D8\n" +
                            "EF            EF                            EF            EF\n" +
-                           "EF            EF                            EF            EF\n" +
-                           "EF            EF     hhhhh       ttttt      EF            EF\n" +
+                           "EF            EF     hhhhh                  EF            EF\n" +
                            "EF   Áccccá   EF     hhhhh       ttttt      EF   Áaaaaá   EF\n" +
-                           "EF   Éccccé   IJ     hhhhh       ttttt      IJ   Éaaaaé   EF\n" +
-                           "EF   Íccccí   EF     hhhhh       ttttt      EF   Íaaaaí   EF\n" +
-                           "EF   Óccccó   EF     hhhhh                  EF   Óaaaaó   EF\n" +
+                           "EF   Éccccé   EF     hhhhh       ttttt      EF   Éaaaaé   EF\n" +
+                           "EF   Íccccí   IJ     hhhhh       ttttt      IJ   Íaaaaí   EF\n" +
+                           "EF   Óccccó   EF     hhhhh       ttttt      EF   Óaaaaó   EF\n" +
                            "EF            EF                            EF            EF\n" +
                            "EF            EF                            EF            EF\n" +
                            "7155555555555541555555995555G5555995555555554155555555555548\n" +
                            "7B66666666666622666666DB6666H6666DB66666666622666666666666D8\n" +
                            "EF                    EF         EF                       EF\n" +
-                           "EF   Ábbbbá  Áaaaaá   EF   eee   EF   Áccccá    ddddd     EF\n" +
+                           "EF                    EF         EF                       EF\n" +
+                           "EF   Ábbbbá  Áaaaaá   EF         EF   Áccccá    ddddd     EF\n" +
                            "EF   Ébbbbé  Éaaaaé   EF   eee   EF   Éccccé    ddddd     EF\n" +
                            "EF   Íbbbbí  Íaaaaí   IJ   eee   IJ   Íccccí    ddddd     EF\n" +
                            "EF   Óbbbbó  Óaaaaó   EF   eee   EF   Óccccó    ddddd     EF\n" +
-                           "EF                    EF         EF                       EF\n" +
+                           "EF                    EF   eee   EF                       EF\n" +
                            "EF                    EF         EF                       EF\n" +
                            "715555555555555555555541555555555415555555555555555555555548\n" +
                            "026666666666666666666622666666666226666666666666666666666623\n";
 
-        string personajes = "1        \n" +
-                            "P -22 -27\n";
+        string personajes = "13       \n" +
+                            "P -22 -27\n" +
+                            "H -5 -21 \n" +
+                            "H -13 -11\n" +
+                            "H -5 20  \n" +
+                            "H -13 10 \n" +
+                            "V -8 -21 \n" +
+                            "V 6 -21  \n" +
+                            "V -16 -11\n" +
+                            "V 17 -11 \n" +
+                            "V -16 17 \n" +
+                            "V 17 17  \n" +
+                            "V -8 27  \n" +
+                            "V 6 27   \n";
 
         string[] lineas = escenario.Split('\n');
         int xtotal = Convert.ToInt32(lineas[0].Split(' ')[0]);
@@ -115,6 +128,7 @@ public class BoardManager : MonoBehaviour
         int e2 = 0;
         int e3 = 0;
         int e4 = 0;
+        int e5 = 0;
         int h = 0;
         int t = 0;
         int p = 0;
@@ -357,6 +371,17 @@ public class BoardManager : MonoBehaviour
 
                         break;
 
+                    case 'i':
+                        GameObject edificio5g = Instantiate(edificio5[e5], new Vector3(xmapa, ymapa, 0f), Quaternion.identity);
+                        instance = Instantiate(acera, new Vector3(xmapa, ymapa, 0f), Quaternion.identity);
+                        e5++;
+                        edificio5g.transform.SetParent(boardHolder3);
+
+                        if (e5 == 25)
+                            e5 = 0;
+
+                        break;
+
                     case 'p':
                         GameObject parqueg = Instantiate(parque[p], new Vector3(xmapa, ymapa, 0f), Quaternion.identity);
                         instance = Instantiate(acera, new Vector3(xmapa, ymapa, 0f), Quaternion.identity);
@@ -394,15 +419,15 @@ public class BoardManager : MonoBehaviour
             switch (ch)
             {
                 case 'P':
-                    instance = Instantiate(player, new Vector3(Convert.ToInt32(jugadores[pl+1].Split(' ')[1]), Convert.ToInt32(jugadores[p + 1].Split(' ')[2]), 0f), Quaternion.identity);
+                    instance = Instantiate(player, new Vector3(Convert.ToInt32(jugadores[pl+1].Split(' ')[1]), Convert.ToInt32(jugadores[pl + 1].Split(' ')[2]), 0f), Quaternion.identity);
                     break;
 
                 case 'V':
-                    instance = Instantiate(enemigov, new Vector3(Convert.ToInt32(jugadores[pl + 1].Split(' ')[1]), Convert.ToInt32(jugadores[p + 1].Split(' ')[2]), 0f), Quaternion.identity);
+                    instance = Instantiate(enemigov, new Vector3(Convert.ToInt32(jugadores[pl + 1].Split(' ')[1]), Convert.ToInt32(jugadores[pl + 1].Split(' ')[2]), 0f), Quaternion.identity);
                     break;
 
                 case 'H':
-                    instance = Instantiate(enemigoh, new Vector3(Convert.ToInt32(jugadores[pl + 1].Split(' ')[1]), Convert.ToInt32(jugadores[p + 1].Split(' ')[2]), 0f), Quaternion.identity);
+                    instance = Instantiate(enemigoh, new Vector3(Convert.ToInt32(jugadores[pl + 1].Split(' ')[1]), Convert.ToInt32(jugadores[pl + 1].Split(' ')[2]), 0f), Quaternion.identity);
                     break;
             }
             //instance.transform.SetParent(boardHolder4);
