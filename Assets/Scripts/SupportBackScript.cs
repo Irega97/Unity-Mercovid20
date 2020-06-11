9ef45bba9b182d30ebaf157ba0d89eb354577af6
cristianat98 <cristianarmestotejedo@gmail.com>
2020-06-11 18:47:19 +0200

    Añadido componente Android

---
+++ Assets/Scripts/SupportBackScript.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class SupportBackScript : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
