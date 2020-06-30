fa8717b20441e54c574d3aaf8b3ed329bcab0799
cristianat98 <cristianarmestotejedo@gmail.com>
2020-06-30 06:12:32 +0200

    Version Final

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -460,6 +460,8 @@ public class GameManager : MonoBehaviour
 
     static private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
     {
+        
+
 #if UNITY_STANDALONE || UNITY_WEBPLAYER || UNITY_EDITOR
         if (instance.nivel == 0)
         {
@@ -469,8 +471,7 @@ public class GameManager : MonoBehaviour
             instance.minutos = 0;
             instance.contador = 0;
             instance.puntos = 0;
-
-           instance.mapa = "60 60 1                                                     \n" +
+            instance.mapa = "60 60 1                                                     \n" +
                            "A9555555555555555555559955555555599599555555555555555555559C\n" +
                            "7B66666666666666666666DB666666666666DB66666666666666666666D8\n" +
                            "EF                    EF            EF                    EF\n" +
@@ -584,7 +585,7 @@ public class GameManager : MonoBehaviour
             SoundManager.instance.curar();
         }
         
-        if (instance.nivel == 1)
+        else if (instance.nivel == 1)
             {
             instance.mapa = "20 16 2             \n" +
                              "33333333 001100117 8\n" +
@@ -913,7 +914,133 @@ public class GameManager : MonoBehaviour
 
  #else
         AndroidJavaClass javaClass = new AndroidJavaClass("edu.upc.login.apiUnity");
-        if (instance.nivel == 1)
+        if (instance.nivel == 0)
+        {
+            instance.codigo = false;
+            instance.llave = false;
+            instance.segundos = 0;
+            instance.minutos = 0;
+            instance.contador = 0;
+            instance.puntos = 0;
+            instance.mapa = javaClass.CallStatic<String>("getMapa", 1);
+            instance.personajes = javaClass.CallStatic<String>("getEnemigos", 1);
+
+            if (instance.mapa == null)
+                instance.mapa = "60 60 1                                                    \n" +
+                           "A9555555555555555555559955555555599599555555555555555555559C\n" +
+                           "7B66666666666666666666DB666666666666DB66666666666666666666D8\n" +
+                           "EF                    EF            EF                    EF\n" +
+                           "EF                    EF            EF                    EF\n" +
+                           "EF   Ábbbbá  Áaaaaá   EF    eee     EF   Áccccá  ddddd    EF\n" +
+                           "EF   Ébbbbé  Éaaaaé   EF    eee     EF   Éccccé  ddddd    EF\n" +
+                           "EF   Íbbbbí  Íaaaaí   IJ    eee     IJ   Íccccí  ddddd    EF\n" +
+                           "EF   Óbbbbó  Óaaaaó   EF    eee     EF   Óccccó  ddddd    EF\n" +
+                           "EF                    EF            EF                    EF\n" +
+                           "EF                    EF            EF                    EF\n" +
+                           "7155555555555599555555415555G5555555415555559955555555555548\n" +
+                           "7B666666666666DB666666226666H666666622666666DB666666666666D8\n" +
+                           "EF            EF                            EF            EF\n" +
+                           "EF            EF   hhhhh                    EF            EF\n" +
+                           "EF   Áccccá   EF   hhhhh       ttttt        EF   Áaaaaá   EF\n" +
+                           "EF   Éccccé   IJ   hhhhh       ttttt        IJ   Éaaaaé   EF\n" +
+                           "EF   Íccccí   EF   hhhhh       ttttt        EF   Íaaaaí   EF\n" +
+                           "EF   Óccccó   EF   hhhhh       ttttt        EF   Óaaaaó   EF\n" +
+                           "EF            EF                            EF            EF\n" +
+                           "EF            EF                            EF            EF\n" +
+                           "71555555555555415555G555555555555555G55555554155555555555548\n" +
+                           "7B666666666666226666H666666666666666H666666622666666666666D8\n" +
+                           "EFiiiii ggggg                            ffff  MMMMMMMMMMMEF\n" +
+                           "EFiiiii ggggg                            fffff MMMMMMMMMMMEF\n" +
+                           "EFiiiii ggggg                            fffff MMMMMMMMMMMEF\n" +
+                           "EFiiiii ggggg                            ffff  MMMMMMMMMMMEF\n" +
+                           "EFiiiii ggggg           pppppppppp                        EF\n" +
+                           "EF                      pppppppppp                        EF\n" +
+                           "EF                      pppppppppp                        EF\n" +
+                           "EF                      pppppppppp                        EF\n" +
+                           "EF                      pppppppppp                        EF\n" +
+                           "EF                      pppppppppp             ggggg iiiiiEF\n" +
+                           "EFddddd ffff            pppppppppp             ggggg iiiiiEF\n" +
+                           "EFddddd fffff           pppppppppp             ggggg iiiiiEF\n" +
+                           "EFddddd fffff                                  ggggg iiiiiEF\n" +
+                           "EFddddd ffff                                   ggggg iiiiiEF\n" +
+                           "EF                                                        EF\n" +
+                           "EF                                                        EF\n" +
+                           "71555555555555995555G5555555555555555555G5559955555555555548\n" +
+                           "7B666666666666DB6666H6666666666666666666H666DB666666666666D8\n" +
+                           "EF            EF                            EF            EF\n" +
+                           "EF            EF     hhhhh                  EF            EF\n" +
+                           "EF   Áccccá   EF     hhhhh       ttttt      EF   Áaaaaá   EF\n" +
+                           "EF   Éccccé   EF     hhhhh       ttttt      EF   Éaaaaé   EF\n" +
+                           "EF   Íccccí   IJ     hhhhh       ttttt      IJ   Íaaaaí   EF\n" +
+                           "EF   Óccccó   EF     hhhhh       ttttt      EF   Óaaaaó   EF\n" +
+                           "EF            EF                            EF            EF\n" +
+                           "EF            EF                            EF            EF\n" +
+                           "7155555555555541555555995555G5555995555555554155555555555548\n" +
+                           "7B66666666666622666666DB6666H6666666DB66666622666666666666D8\n" +
+                           "EF                    EF            EF                    EF\n" +
+                           "EF                    EF            EF                    EF\n" +
+                           "EF   Ábbbbá  Áaaaaá   EF    eee     EF   Áccccá  ddddd    EF\n" +
+                           "EF   Ébbbbé  Éaaaaé   EF    eee     EF   Éccccé  ddddd    EF\n" +
+                           "EF   Íbbbbí  Íaaaaí   IJ    eee     IJ   Íccccí  ddddd    EF\n" +
+                           "EF   Óbbbbó  Óaaaaó   EF    eee     EF   Óccccó  ddddd    EF\n" +
+                           "EF                    EF            EF                    EF\n" +
+                           "EF                    EF            EF                    EF\n" +
+                           "715555555555555555555541555555555555415555555555555555555548\n" +
+                           "026666666666666666666622666666666666226666666666666666666623\n";
+
+            if (instance.personajes == null)
+                instance.personajes = "48      \n" +
+                            "P 7 3 1            \n" +
+                            "0 0 0 23 11 0 0    \n" +
+                            "1 0 48 23 11 0 0   \n" +
+                            "2 36 48 23 11 0 0  \n" +
+                            "3 36 0 23 11 0 0   \n" +
+                            "4 22 0 15 11 0 0   \n" +
+                            "0 0 10 15 11 0 0   \n" +
+                            "1 44 10 15 11 0 0  \n" +
+                            "2 44 38 15 11 0 0  \n" +
+                            "3 0 38 15 11 0 0   \n" +
+                            "4 22 48 15 11 0 0  \n" +
+                            "0 0 20 59 19 0 0   \n" +
+                            "1 22 1 21 9 2 1    \n" +
+                            "2 58 1 21 9 2 1    \n" +
+                            "3 58 49 21 9 2 1   \n" +
+                            "4 22 49 21 9 2 1   \n" +
+                            "0 58 39 13 9 2 1   \n" +
+                            "1 36 1 13 9 2 1    \n" +
+                            "2 58 11 13 9 2 1   \n" +
+                            "3 14 11 13 9 2 1   \n" +
+                            "4 14 39 13 9 2 1   \n" +
+                            "0 36 49 13 9 2 1   \n" +
+                            "1 58 21 57 17 2 1  \n" +
+                            "2 14 10 31 11 0 0  \n" +
+                            "3 44 11 29 9 2 1   \n" +
+                            "4 14 38 31 11 0 0  \n" +
+                            "0 44 39 29 9 2 1   \n" +
+                            "H 24 9   \n" +
+                            "H 2 22   \n" +
+                            "H 2 30   \n" +
+                            "H 57 30  \n" +
+                            "H 16 40  \n" +
+                            "H 24 50  \n" +
+                            "H 40 37  \n" +
+                            "H 16 19  \n" +
+                            "H 43 12  \n" +
+                            "H 43 47  \n" +
+                            "V 21 2   \n" +
+                            "V 38 9   \n" +
+                            "V 13 12  \n" +
+                            "V 46 19  \n" +
+                            "V 23 37  \n" +
+                            "V 34 22  \n" +
+                            "V 13 47  \n" +
+                            "V 21 50  \n" +
+                            "V 46 40  \n" +
+                            "V 38 57  \n" +
+                            "L 46 37  \n";
+            SoundManager.instance.curar();
+        }
+        else if (instance.nivel == 1)
         {
             instance.mapa = javaClass.CallStatic<String>("getMapa", 2);
             instance.personajes = javaClass.CallStatic<String>("getEnemigos", 2);
